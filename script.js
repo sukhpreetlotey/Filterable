16398f8b14e50ac1d7c3f822ba18de590e83d71b
sukhpreetlotey <116991911+sukhpreetlotey@users.noreply.github.com>
2022-10-31 18:01:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,175 @@
+// All
+
+document.getElementsByClassName("btn")[0].addEventListener("click", function() {
+    document.getElementsByTagName("img")[0].src = "pics/mob2.jpg";
+    document.getElementsByTagName("img")[1].src = "pics/lapi4.jpg";
+    document.getElementsByTagName("img")[2].src = "pics/tab4.jpg";
+    document.getElementsByTagName("img")[3].src = "pics/head4.jpg";
+    document.getElementsByTagName("img")[4].src = "pics/mob4.jpg";
+    document.getElementsByTagName("img")[5].src = "pics/mob5.jpg";
+    document.getElementsByTagName("img")[6].src = "pics/tab6.jpg";
+    document.getElementsByTagName("img")[7].src = "pics/mob1.jpg";
+    document.getElementsByTagName("img")[8].src = "pics/mob6.jpg";
+    document.getElementsByTagName("img")[9].src = "pics/head1.jpg";
+    document.getElementsByTagName("img")[10].src = "pics/tab1.jpg";
+    document.getElementsByTagName("img")[11].src = "pics/mob3.jpg";
+    document.getElementsByTagName("img")[12].src = "pics/head5.jpg";
+    document.getElementsByTagName("img")[13].src = "pics/lapi7.jpg";
+    document.getElementsByTagName("img")[14].src = "pics/head6.jpg";
+    document.getElementsByTagName("img")[15].src = "pics/lapi1.jpg";
+    document.getElementsByTagName("img")[16].src = "pics/lapi3.jpg";
+    document.getElementsByTagName("img")[17].src = "pics/head3.jpg";
+    document.getElementsByTagName("img")[18].src = "pics/lapi5.jpg";
+    document.getElementsByTagName("img")[19].src = "pics/mob7.jpg";
+    document.getElementsByTagName("img")[20].src = "pics/lapi6.jpg";
+    document.getElementsByTagName("img")[21].src = "pics/tab2.jpg";
+    document.getElementsByTagName("img")[22].src = "pics/head2.jpg";
+    document.getElementsByTagName("img")[23].src = "pics/lapi2.jpg";
+    document.getElementsByTagName("img")[24].src = "pics/tab3.jpg";
+    document.getElementsByTagName("img")[25].src = "pics/tab5.jpg";
+})
+
+// Mobiles
+
+document.getElementsByClassName("btn")[1].addEventListener("click", function() {
+    document.getElementsByTagName("img")[0].src = "pics/mob1.jpg";
+    document.getElementsByTagName("img")[1].src = "pics/mob2.jpg";
+    document.getElementsByTagName("img")[2].src = "pics/mob3.jpg";
+    document.getElementsByTagName("img")[3].src = "pics/mob4.jpg";
+    document.getElementsByTagName("img")[4].src = "pics/mob5.jpg";
+    document.getElementsByTagName("img")[5].src = "pics/mob6.jpg";
+    document.getElementsByTagName("img")[6].src = "pics/mob7.jpg";
+    document.getElementsByTagName("img")[7].src = "";
+    document.getElementsByTagName("img")[8].src = "";
+    document.getElementsByTagName("img")[9].src = "";
+    document.getElementsByTagName("img")[10].src = "";
+    document.getElementsByTagName("img")[11].src = "";
+    document.getElementsByTagName("img")[12].src = "";
+    document.getElementsByTagName("img")[13].src = "";
+    document.getElementsByTagName("img")[14].src = "";
+    document.getElementsByTagName("img")[15].src = "";
+    document.getElementsByTagName("img")[16].src = "";
+    document.getElementsByTagName("img")[17].src = "";
+    document.getElementsByTagName("img")[18].src = "";
+    document.getElementsByTagName("img")[19].src = "";
+    document.getElementsByTagName("img")[20].src = "";
+    document.getElementsByTagName("img")[21].src = "";
+    document.getElementsByTagName("img")[22].src = "";
+    document.getElementsByTagName("img")[23].src = "";
+    document.getElementsByTagName("img")[24].src = "";
+    document.getElementsByTagName("img")[25].src = "";
+    document.getElementsByTagName("img")[26].src = "";
+})
+
+// Tablets
+
+document.getElementsByClassName("btn")[2].addEventListener("click", function() {
+    document.getElementsByTagName("img")[0].src = "pics/tab1.jpg";
+    document.getElementsByTagName("img")[1].src = "pics/tab2.jpg";
+    document.getElementsByTagName("img")[2].src = "pics/tab3.jpg";
+    document.getElementsByTagName("img")[3].src = "pics/tab4.jpg";
+    document.getElementsByTagName("img")[4].src = "pics/tab5.jpg";
+    document.getElementsByTagName("img")[5].src = "pics/tab6.jpg";
+    document.getElementsByTagName("img")[6].src = "";
+    document.getElementsByTagName("img")[7].src = "";
+    document.getElementsByTagName("img")[8].src = "";
+    document.getElementsByTagName("img")[9].src = "";
+    document.getElementsByTagName("img")[10].src = "";
+    document.getElementsByTagName("img")[11].src = "";
+    document.getElementsByTagName("img")[12].src = "";
+    document.getElementsByTagName("img")[13].src = "";
+    document.getElementsByTagName("img")[14].src = "";
+    document.getElementsByTagName("img")[15].src = "";
+    document.getElementsByTagName("img")[16].src = "";
+    document.getElementsByTagName("img")[17].src = "";
+    document.getElementsByTagName("img")[18].src = "";
+    document.getElementsByTagName("img")[19].src = "";
+    document.getElementsByTagName("img")[20].src = "";
+    document.getElementsByTagName("img")[21].src = "";
+    document.getElementsByTagName("img")[22].src = "";
+    document.getElementsByTagName("img")[23].src = "";
+    document.getElementsByTagName("img")[24].src = "";
+    document.getElementsByTagName("img")[25].src = "";
+    document.getElementsByTagName("img")[26].src = "";
+})
+
+// Headphones
+
+document.getElementsByClassName("btn")[3].addEventListener("click", function() {
+    document.getElementsByTagName("img")[0].src = "pics/head1.jpg";
+    document.getElementsByTagName("img")[1].src = "pics/head2.jpg";
+    document.getElementsByTagName("img")[2].src = "pics/head3.jpg";
+    document.getElementsByTagName("img")[3].src = "pics/head4.jpg";
+    document.getElementsByTagName("img")[4].src = "pics/head5.jpg";
+    document.getElementsByTagName("img")[5].src = "pics/head6.jpg";
+    document.getElementsByTagName("img")[6].src = "";
+    document.getElementsByTagName("img")[7].src = "";
+    document.getElementsByTagName("img")[8].src = "";
+    document.getElementsByTagName("img")[9].src = "";
+    document.getElementsByTagName("img")[10].src = "";
+    document.getElementsByTagName("img")[11].src = "";
+    document.getElementsByTagName("img")[12].src = "";
+    document.getElementsByTagName("img")[13].src = "";
+    document.getElementsByTagName("img")[14].src = "";
+    document.getElementsByTagName("img")[15].src = "";
+    document.getElementsByTagName("img")[16].src = "";
+    document.getElementsByTagName("img")[17].src = "";
+    document.getElementsByTagName("img")[18].src = "";
+    document.getElementsByTagName("img")[19].src = "";
+    document.getElementsByTagName("img")[20].src = "";
+    document.getElementsByTagName("img")[21].src = "";
+    document.getElementsByTagName("img")[22].src = "";
+    document.getElementsByTagName("img")[23].src = "";
+    document.getElementsByTagName("img")[24].src = "";
+    document.getElementsByTagName("img")[25].src = "";
+    document.getElementsByTagName("img")[26].src = "";
+})
+
+// Laptops
+
+document.getElementsByClassName("btn")[4].addEventListener("click", function() {
+    document.getElementsByTagName("img")[0].src = "pics/lapi1.jpg";
+    document.getElementsByTagName("img")[1].src = "pics/lapi2.jpg";
+    document.getElementsByTagName("img")[2].src = "pics/lapi3.jpg";
+    document.getElementsByTagName("img")[3].src = "pics/lapi4.jpg";
+    document.getElementsByTagName("img")[4].src = "pics/lapi5.jpg";
+    document.getElementsByTagName("img")[5].src = "pics/lapi6.jpg";
+    document.getElementsByTagName("img")[6].src = "pics/lapi7.jpg";
+    document.getElementsByTagName("img")[7].src = "";
+    document.getElementsByTagName("img")[8].src = "";
+    document.getElementsByTagName("img")[9].src = "";
+    document.getElementsByTagName("img")[10].src = "";
+    document.getElementsByTagName("img")[11].src = "";
+    document.getElementsByTagName("img")[12].src = "";
+    document.getElementsByTagName("img")[13].src = "";
+    document.getElementsByTagName("img")[14].src = "";
+    document.getElementsByTagName("img")[15].src = "";
+    document.getElementsByTagName("img")[16].src = "";
+    document.getElementsByTagName("img")[17].src = "";
+    document.getElementsByTagName("img")[18].src = "";
+    document.getElementsByTagName("img")[19].src = "";
+    document.getElementsByTagName("img")[20].src = "";
+    document.getElementsByTagName("img")[21].src = "";
+    document.getElementsByTagName("img")[22].src = "";
+    document.getElementsByTagName("img")[23].src = "";
+    document.getElementsByTagName("img")[24].src = "";
+    document.getElementsByTagName("img")[25].src = "";
+    document.getElementsByTagName("img")[26].src = "";
+})
+
+
+
+// select image
+
+let fullImgBox = document.getElementById("fullImgBox");
+let fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic) {
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+}
+
+function closeFullImg() {
+    fullImgBox.style.display = "none";
+
+}
